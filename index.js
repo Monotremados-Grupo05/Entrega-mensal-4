84574f93b2f90674547d75361e36804130f2ba9c
Nicolas <nicolasyuj@hotmail.com>
2022-12-14 16:39:10 -0300

    Adicioneiumascoisas

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ const options = {
 const dive = document.getElementById("dive");  //variavel que recebe o getelementbyid para nao ter que ficar escrevendo isso toda vez
 var a = 0;                                     //variavel para controle de quantidade de jogos que irao aparecer na tela
 var b = 10;                                    //variavel para controle de quantidade de jogos que irao aparecer na tela
-var filtro_escolhido=""
-
-
+var filtro_escolhido="";
+var plataforma_escolhida="all";
 var favoritos = JSON.parse(localStorage.getItem('favs')) || [];
-var resultados = []
-
+var resultados = [];
+const name = document.querySelector(".textopc");
 function updateLocalStorage() {
     localStorage.setItem('favs', JSON.stringify(favoritos));
 }
@@ -27,8 +26,6 @@ function isInFavorite(id) {
     }
     return false
 }
-
-
 
 function handleFavClick(index, btnid) {
    for(i=0; i < favoritos.length; i++) {
@@ -45,11 +42,8 @@ function handleFavClick(index, btnid) {
 
 }
 
-
-const name = document.querySelector(".textopc");
-
-function consultaDadosViaCep(){                //funcao que realiza o fetch
-    fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games${filtro_escolhido}`, options)
+function ConsultarJogos(){                //funcao que realiza o fetch
+    fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?platform=${plataforma_escolhida}${filtro_escolhido}`, options)
 	.then((response) => {
         response.json()                        //Transformo os dados em json
             .then(data => printar(data));      // envio os dados chamando funcao printar indicando o parametro data (sendo os proprios dados)
@@ -63,119 +57,148 @@ const printar = (resultado) =>{                //Funcao que printa os dados da A
         console.log(resultado[a]);       //o botao os valores das variaveis irao ser alteradas para que seja printado mais
         dive.innerHTML += `
         <div class="vitrine">
-        <img class="fotojogo" src="${resultado[a].thumbnail}">
+        <a href="${resultado[a].freetogame_profile_url}"target="_blank">
+        <img class="fotojogo" src="${resultado[a].thumbnail}"></a>
         <div class="card-base-container"> 
         <p class="nomedojogo">${resultado[a].title}</p> 
         <button class="like-btn" onClick="handleFavClick(${a},${resultado[a].id})">
-            <svg id="${resultado[a].id}" 
-            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="${isInFavorite(resultado[a].id) ? "#FFFFFF" : "none"}" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-            </svg> 
-        </button>
-
-            
+        <svg id="${resultado[a].id}" 
+        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="${isInFavorite(resultado[a].id) ? "#FFFFFF" : "none"}" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+        </svg> 
+        </button>    
         <div>
                            
         </div>`
-    }        
-                                          //Dentro do for eu uso a variavel dive e altero o conteudo do 
-    }                                          //HTML dela, inserindo o nome do jogo,imagem e link para o site      
-    function apagar(){
-        dive.innerHTML = "";                  //Funcao para apagar o conteudo escrito dentro do HTML da variavel dive, utilizado para caso
-        a=0; b=10;
-    }                                         //o usuario modifique o padrao de aparecimento sei la vai apagar tudo e escrever de outro jeito
+    }         
+}
+      
+function apagar(){
+    dive.innerHTML = "";                  //Funcao para apagar o conteudo escrito dentro do HTML da variavel dive, utilizado para caso
+    a=0; b=10;
+}                                         //o usuario modifique o padrao de aparecimento sei la vai apagar tudo e escrever de outro jeito
 
-    consultaDadosViaCep();                   //Aqui nesse caso eu to chamando a funcao apenas
+ConsultarJogos();                   //Aqui nesse caso eu to chamando a funcao apenas
 
-    function adicionar(){                   //Funcao adicionar para alterar as variaveis que definem a quantidade de jogos que irao aparecer
+function adicionar(){                   //Funcao adicionar para alterar as variaveis que definem a quantidade de jogos que irao aparecer
     a = b;                                  // na tela do usuario, ela eh chamada ao clicar no botao carregar mais
     b = b+10;                               //Alem de alterar as variaveis ela chama a funcao consultardadosviacep pra printar novamente
-    consultaDadosViaCep();
+    ConsultarJogos();
 }
+
 function filtrar(filtro_botao){
-    console.log(filtro_botao);
-
     switch(filtro_botao){
-    
+        case('Home'):
+            filtro_escolhido = "&sort-by=popularity";
+            apagar();
+            ConsultarJogos();
+        break;
+        
         case('Moba'):
-            filtro_escolhido = "?category=moba";
+            filtro_escolhido = "&category=moba";
             apagar();
-            consultaDadosViaCep();
+            ConsultarJogos();
         break;
-
+    
         case('Survival'):
-            filtro_escolhido = "?category=survival";
+            filtro_escolhido = "&category=survival";
             apagar();
-            consultaDadosViaCep();
+            ConsultarJogos();
         break;
-
+    
         case('Fighting'):
-            filtro_escolhido = "?category=fighting";
+            filtro_escolhido = "&category=fighting";
             apagar();
-            consultaDadosViaCep();
+            ConsultarJogos();
         break;
-    
+        
         case('Shooting'):
-        filtro_escolhido = "?category=shooter";
-        apagar();
-        consultaDadosViaCep();
-        break;
-
-        case('Fantasy'):
-        filtro_escolhido = "?category=fantasy";
-        apagar();
-        consultaDadosViaCep();
-        break;
-
-        case('Card game'):
-        filtro_escolhido = "?category=card";
-        apagar();
-        consultaDadosViaCep();
-        break;
-
-        case('Strategy'):
-            filtro_escolhido = "?category=strategy";
+            filtro_escolhido = "&category=shooter";
             apagar();
-            consultaDadosViaCep();
+            ConsultarJogos();
         break;
-
-        case('Sports'):
-        filtro_escolhido = "?category=sports";
-        apagar();
-        consultaDadosViaCep();
-        break;
-
-
-    }
     
+        case('Fantasy'):
+            filtro_escolhido = "&category=fantasy";
+            apagar();
+            ConsultarJogos();
+        break;
+    
+        case('Card game'):
+            filtro_escolhido = "&category=card";
+            apagar();
+            ConsultarJogos();
+        break;
+    
+        case('Strategy'):
+            filtro_escolhido = "&category=strategy";
+            apagar();
+            ConsultarJogos();
+        break;
+    
+        case('Sports'):
+            filtro_escolhido = "&category=sports";
+            apagar();
+            ConsultarJogos();
+        break;
+    }
 } 
+
+function plataforma(filtro_plataforma){
+    switch(filtro_plataforma){
+        case('PC'):
+        plataforma_escolhida="pc";
+        console.log(plataforma_escolhida);
+        apagar();
+        ConsultarJogos();
+        break;
+
+        case('Browser'):
+        plataforma_escolhida="browser";
+        apagar();
+        ConsultarJogos();
+        break;
+
+        case('ALL'):
+        plataforma_escolhida="all";
+        apagar();
+        ConsultarJogos();
+        break;
+    }
+}
 
 function mostrarAtivo(tag){
     var tag_li = document.getElementById('lista_menu');
     var tag_a = tag_li.getElementsByTagName('a');
-    for (i=0; i<tag_a.length; i++ )
-    {
-       tag_a[i].style.backgroundColor = "";
+    for (i=0; i<tag_a.length; i++ ){
+        tag_a[i].style.backgroundColor = "";
     }
-       tag.style.backgroundColor = "#660920";
-       
+    tag.style.backgroundColor = "#660920";       
 }
 
-//
+function mostrarAtivo_plat(tag2){
+    var tag2_li2 = document.getElementById('Ulnav');
+    var tag2_a2 = tag2_li2.getElementsByTagName('a');
+    for (i=0; i<tag2_a2.length; i++ ){
+        tag2_a2[i].style.backgroundColor = "";
+    }
+    tag2.style.backgroundColor = "#3D0C02";
+    tag2.style.color = "white";   
+}
 
-const favoriteslink = document.getElementById("favorites-link")
+const favoriteslink = document.getElementById("favorites-link");
+
 favoriteslink.addEventListener("click", () => {
     apagar();
-    filtro_escolhido = "favoritos"
-    printar(favoritos)
+    filtro_escolhido = "favoritos";
+    printar(favoritos);
 });
-
 
 window.onscroll = function() {
     if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
         if( filtro_escolhido != "favoritos") {
-            adicionar()
+            adicionar();
         }
         
     }
-   }
+}
